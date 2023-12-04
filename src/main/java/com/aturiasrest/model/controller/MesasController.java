cda7573c6d75b6c03b142455264205d25be377af
jampierjrl <jam.dta18@gmail.com>
2023-12-03 21:21:03 -0500

    modifique put en update de mesa

--- src/main/java/com/aturiasrest/model/controller/MesasController.java
+++ src/main/java/com/aturiasrest/model/controller/MesasController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,7 +30,7 @@ public class MesasController {
 		return new ResponseEntity<>(mesasService.srvGuardarMesa(mesas), HttpStatus.CREATED);
 	}
 
-	@PostMapping("/update/{id}")
+	@PutMapping("/update/{id}")
 	public ResponseEntity<?> Mesa(@RequestBody MesaModel mesas,
 			@PathVariable("id") int codMesa) {
 		return ResponseEntity.ok().body(mesasService.srvActualizarMesa(mesas, codMesa));
